5be0157c3f24dce775a54747257c2dce5b51ad1e
AbdulMalik9405 <abdulmaliktojahmedov9405@gmail.com>
2024-11-29 10:18:15 +0900

    Summary of Seating_chart

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 # Define color codes (ANSI Format)
 GREEN = "\033[32m"    # Available seat
 RED = "\033[31m"      # booked seat
-YELLOW = "\033[32m"   # Reserved seat
+YELLOW = "\033[34m"   # Reserved seat
 BLUE = "\033[34m"     # VIP seat
 RESET = "\033[39m"    # Reset color
 
@@ -76,20 +76,52 @@ def open_file():
             seating_chart[i][j] = temp_file[j]
     file.close
 
+def change_seat(row, column):
+    global Available, Booked, Reserved, VIP
+    if seating_chart[row][column] == "A":
+        Available = Available - 1
+    elif seating_chart[row][column] == "B":
+        Booked = Booked - 1
+    elif seating_chart[row][column] == "R":
+        Reserved = Reserved - 1
+    else:
+        VIP = VIP - 1
+
+Available = 0
+Booked = 0
+Reserved = 0
+VIP = 0
+
 # Main program loop
 def main():
-    loop = True
-    while loop == True:
+    global Available, Booked, Reserved, VIP
+    previous_chart = True
+    while previous_chart == True:
         previous = input("Would you like to work with the previous seating list? Enter yes or no: ")
         previous = previous.lower()
         if previous == "yes":
+            previous_chart = False
             open_file()
             display_chart()
         elif previous == "no":
+            previous_chart = False
             display_chart()
         else:
             print("Invalid input")
-            continue
+            previous_chart = True
+    for i in range(5):
+        for j in range(5):
+            if seating_chart[i][j] == "A":
+                Available = Available + 1
+            elif seating_chart[i][j] == "B":
+                Booked = Booked + 1
+            elif seating_chart[i][j] == "R":
+                Reserved = Reserved + 1
+            else:
+                VIP = VIP + 1
+    loop = True
+    while loop == True:
+        print("There are ", Available, " number of seats available. ", Booked, "Number of seats booked. ",Reserved, "Number of seats reserved.", VIP, "Number of VIP seats." )
         action = input("Are you booking, reserving, marking a seat as VIP or Freeing up a seat. Enter booking, reserving, vip or free: ")
         action = action.lower()
         if action == "booking":
@@ -97,29 +129,37 @@ def main():
             row = row - 1
             column = int(input("Which column would you like to book? 1-5"))
             column = column - 1
+            change_seat(row, column)
             book_seat(row, column)
             save_file()
+            Booked = Booked + 1
         elif action == "reserving":
             row  = int(input("Which row would you like to reserve? 1-5"))
             row = row - 1
             column = int(input("Which column would you like to reserve? 1-5"))
             column = column - 1
+            change_seat(row, column)
             reserve_seat(row, column)
             save_file()
+            Reserved = Reserved + 1
         elif action == "vip":
             row  = int(input("Which row would you like to mark as vip? 1-5"))
             row = row - 1
             column = int(input("Which column would you like to mark as vip? 1-5"))
             column = column - 1
+            change_seat(row, column)
             vip_seat(row, column)
             save_file()
+            VIP = VIP + 1
         elif action == "free":
             row  = int(input("Which row would you like to free? 1-5"))
             row = row - 1
             column = int(input("Which column would you like to free? 1-5"))
             column = column - 1
+            change_seat(row, column)
             free_seat(row, column)
             save_file()
+            Available = Available + 1
         else:
             print("Invalid input")
             continue
@@ -132,4 +172,5 @@ def main():
         else:
             continue
         display_chart()
+
 main()
